aee84b0f7ed36a99090cb271a2f49a079f45ad1e
Jesse Winters <jmwinters@email.neit.edu>
2017-10-02 14:41:27 -0400

    Added Table

--- mult.php
+++ mult.php
@@ -1,3 +1,15 @@
+<?php
+$table = "<table>";
+$table .= "</table>";
+
+
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +17,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<?php echo $table; ?>
 </body>
 </html>
