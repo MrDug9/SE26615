cc3a2f3fb9f561c4bfef73657fc5c6eb23f33159
Jesse Winters <jmwinters@email.neit.edu>
2017-10-02 14:53:55 -0400

    Added cells

--- mult.php
+++ mult.php
@@ -1,7 +1,11 @@
 <?php
 $table = "<table>\n";
-for ($rows =0;$rows<9;$rows++){
+for ($rows =1;$rows<=9;$rows++){
     $table .= "\t<tr>";
+    for($cols=1;$cols<=9;$cols++){
+        $table .= "<td>".$rows * $cols."</td>";
+
+    }
     $table .= "</tr>\n";
 
 }
