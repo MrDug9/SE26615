3dfcabe863e757a67bb9f6c7f344f603d546e930
Jesse Winters <jmwinters@email.neit.edu>
2017-10-02 14:11:07 -0400

    update

--- lab1.php
+++ lab1.php
@@ -3,6 +3,9 @@
  * Created by PhpStorm.
  * User: 001317919
  * Date: 10/2/2017
- * Time: 1:44 PM
+ * Time: 1:49 PM
  */
-hi
+$firstName = "jesse";
+$lastName = "winters";
+
+echo $firstName." ".$lastName;
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001317919
+ * Date: 10/2/2017
+ * Time: 2:01 PM
+ */
