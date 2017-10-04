8c85d508e4685bd25a1c69a448a9a5cdde4089bc
Jesse Winters <jmwinters@email.neit.edu>
2017-10-04 13:10:24 -0400

    start of class 2

--- mult.php
+++ mult.php
@@ -1,8 +1,8 @@
 <?php
 $table = "<table>\n";
-for ($rows =1;$rows<=9;$rows++){
+for ($rows =1;$rows<=10;$rows++){
     $table .= "\t<tr>";
-    for($cols=1;$cols<=9;$cols++){
+    for($cols=1;$cols<=10;$cols++){
         $table .= "<td>".$rows * $cols."</td>";
 
     }
@@ -23,7 +23,7 @@ $table .= "</table>\n";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Random Grid</title>
 </head>
 <body>
 <?php echo $table; ?>
