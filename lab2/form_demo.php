0751c3fb8e61b93193de493b774d9f4b140327ae
Jesse Winters <jmwinters@email.neit.edu>
2017-10-11 14:55:57 -0400

    db made named test

--- lab2/form_demo.php
+++ lab2/form_demo.php
@@ -10,15 +10,41 @@ $username = "test";
 $password = "se266";
 try {
 $db = new PDO($dsn, $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     die("There is problem. GO AWAY!!!");
 }
+/*if ( isset($_POST['submit'])){
+    $submit = $_POST['submit'];
+}else{
+    $submit = "";
+}*/
+//$submit = isset($_POST['submit']) ? $_POST['submit'] : ""; //ternary
+
+$submit = $_POST['submit'] ?? ""; //null coalescence operator
+if($submit == "Do It"){
+    $name = $_POST['name'] ?? "";
+    $gender = $_POST['gender'] ?? "NULL";
+    $fixed = $_POST['fixed'] ?? false;
+    try {
+        $sql = $db->prepare("INSERT INTO animals VALUES(null, :name, :gender, :fixed)");
+        $sql->bindParam(':name', $name);
+        $sql->bindParam(':gender', $gender);
+        $sql->bindParam(':fixed', $fixed);
+        $sql->execute();
+        echo $sql->rowCount() . " rows inserted.";
+    } catch(PDOException $e){
+        $e->getMessage();
+    }
+}
+
+
 
 ?>
 <form method="post" action="#">
     name: <input type="text" name="name" /><br />
     gender: M <input type="radio" name="gender" value="M" />
-    F <input type="radio" name="gender" value="M" /><br />
-    fixed <input type="checkbox" name="fixed" value="T" />
+    F <input type="radio" name="gender" value="F" /><br />
+    fixed <input type="checkbox" name="fixed" value="true" />
     <input type="submit" name="submit" value="Do It" />
 </form>
