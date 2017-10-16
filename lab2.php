48581f2ed82926eaa191ee398809f3d50be9cea7
Jesse Winters <jmwinters@email.neit.edu>
2017-10-16 11:27:55 -0400

    start of class wk3

--- lab2.php
+++ lab2.php
@@ -5,3 +5,20 @@
  * Date: 10/11/2017
  * Time: 1:10 PM
  */
+$dsn = "mysql:host=localhost; dbname=phpclassfall2017";
+$username = "phpclassfall2017";
+$password = "classpass";
+
+try {
+    $db = new PDO($dsn, $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    die("There is problem. GO AWAY!!!");
+}
+
+
+
+
+
+
+?>
