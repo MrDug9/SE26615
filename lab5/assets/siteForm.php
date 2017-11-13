c6f9245771f205b2eff615f52c56a66ad17af2b0
Jesse Winters <jmwinters@email.neit.edu>
2017-11-13 12:33:59 -0500

    start lab5

---
+++ lab5/assets/siteForm.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001317919
+ * Date: 11/13/2017
+ * Time: 12:15 PM
+ */
+?>
+<form method="post" action="#">
+    <label for="site">Enter URL</label><input type="text" name="site"/><br />
+    <input type="submit" name="action" value="submit" />
+</form>
---
+++ lab5/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001317919
+ * Date: 11/13/2017
+ * Time: 12:11 PM
+ */
+require_once ("assets/dbcon.php");
+
+
+include_once ("assets/siteForm.php");
+
+
+
+
+
+
+
+
