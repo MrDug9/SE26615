a9beb52cad0943e02b8ac3d76a9b82b34ad70a76
Jesse Winters <jmwinters@email.neit.edu>
2017-11-27 13:27:02 -0500

    lab 5 done

--- lab5/assets/siteForm.php
+++ lab5/assets/siteForm.php
@@ -9,19 +9,64 @@
 <form method="post" action="#">
     <label for="site">Enter URL</label><input type="text" name="site"/><br />
     <input type="submit" name="action" value="submit" />
+    <?php
+    echo(makeDD(dbcon()))
+    ?>
+
+    <input type="submit" name="action" value="search" />
 </form>
 
 <?php
 
-function siteFind($URL){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $URL);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $out = curl_exec($ch);
-    curl_close($ch);
-    return($out);
+function siteFind($db,$URL){
+    try {
+        $sql = $db->prepare("SELECT * FROM sites WHERE site= :url");
+        $sql->bindParam(':url', $URL);
+        $sql->execute();
+
+        if ($sql->rowCount() < 1) {
+            $out = 0;
+        } else {
+            $out = 1;
+        }
+        return ($out);
+    }catch (PDOException $e){
+        die($e);
+    }
 }
 
-function
+function addSite($db, $URL){
+    try {
+        $sql = $db->prepare("INSERT INTO sites VALUES(null, :site, now())");
+        $sql->bindParam(':site', $URL);
+        $sql->execute();
+        echo $sql->rowCount() . " rows inserted.";
+    } catch(PDOException $e){
+        $e->getMessage();
+    }
+}
+
+function makeDD($db){
+    try {
+        $sql = $db->prepare("SELECT * FROM sites WHERE 0=0");
+        $sql->execute();
+        $sites = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($sql->rowCount() > 0) {
+            $dd = "<select name='ddsite'><option value=''>Select A Site</option>";
+            foreach ($sites as $site){
+                $dd.= "<option value='".$site['site']."'>".$site['site']."</option>";
+            }
+            $dd.="</select>".PHP_EOL;
+        } else {
+            $dd = "ERROR";
+        }
+        return $dd;
+    }catch (PDOException $e){
+        die("problem with Table");
+    }
+}
+
+
 
 ?>
--- lab5/index.php
+++ lab5/index.php
@@ -12,14 +12,14 @@ require_once ("assets/dbcon.php");
 
 
 include_once ("assets/siteForm.php");
+if(isset($_POST['action'])) {
 
-if(isset($_POST['action'])){
-
-        if(!preg_match("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/",$_POST['site'])){
+    if($_POST['action'] == "submit") {
+        if (!preg_match("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/", $_POST['site'])) {
             echo "Please enter a valid website";
-        }else{
-            //$sitesFound = siteFind(dbcon(),$_POST['site']);
-            //if($sitesFound == 0) {
+        } else {
+            $sitesFound = siteFind(dbcon(), $_POST['site']);
+            if ($sitesFound == 0) {
                 $sites = array();
                 $file = file_get_contents($_POST['site']);
                 echo "<b>" . preg_match_all("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/", $file, $matches, PREG_OFFSET_CAPTURE) . " link(s) found for </b>\"<a href='" . $_POST['site'] . "'>" . $_POST['site'] . "</a>\"<br><hr>";
@@ -27,17 +27,31 @@ if(isset($_POST['action'])){
                 foreach ($matches as $match) {
                     foreach ($match as $m) {
                         array_push($sites, $m[0]);
-                        echo "<a href='".$m[0]."'>".$m[0] . "</a><br>";
+                        echo "<a href='" . $m[0] . "'>" . $m[0] . "</a><br>";
                     }
                 }
-               // addSite(dbcon(), $_POST['site'], $sites);
-            //}else{
-            //    echo $_POST['site']." already exists in the database<br>";
-            //    echo $sitesFound;
+                addSite(dbcon(), $_POST['site']);
+            } else {
+                echo $_POST['site'] . " already in the database<br>";
+                echo $sitesFound;
             }
         }
+    }
+    if($_POST['action'] == "search") {
 
+        $sites = array();
+        $file = file_get_contents($_POST['ddsite']);
+        echo "<b>" . preg_match_all("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/", $file, $matches, PREG_OFFSET_CAPTURE) . " link(s) found for </b>\"<a href='" . $_POST['ddsite'] . "'>" . $_POST['site'] . "</a>\"<br><hr>";
+        preg_match_all("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/", $file, $matches, PREG_OFFSET_CAPTURE);
+        foreach ($matches as $match) {
+            foreach ($match as $m) {
+                array_push($sites, $m[0]);
+                echo "<a href='" . $m[0] . "'>" . $m[0] . "</a><br>";
+            }
+        }
+    }
 
+}
 
 
 
